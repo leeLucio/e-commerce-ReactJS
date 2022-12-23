536af3e369dae3d482ab42613360b4384ee6fa91
Lucio <lucio.a00@gmail.com>
2022-12-23 19:32:44 -0300

    Sacar imports innecesarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Titulo from './componentes/Titulo/Titulo'
 import NavBar from './componentes/NavBar/NavBar'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 
