aae33c0288da6c2d0ff05f547e78b230b6f35123
Lucio <lucio.a00@gmail.com>
2023-01-21 13:34:00 -0300

    Agregar contador para los productos

---
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ItemCount = ({ initial = 1, stock = 10, onAdd }) => {
+	const [count, setCount] = useState(initial)
+
+	const aumentarCount = () => {
+		count < stock && setCount(count + 1)
+	}
+
+	const disminuirCount = () => {
+		count > 1 && setCount(count - 1)
+	}
+
+	const handleOnAdd = () =>{
+		count <= stock && onAdd(count)
+	}
+
+	return (
+		<div className="card">
+			<div className="card-body row">
+				<div className="col">
+					<button className=' btn btn-outline-dark w-100' onClick={disminuirCount}>-</button>
+				</div>
+				<div className="col">
+					<p className='w-100'>{count}</p>
+				</div>
+				<div className="col">
+					<button className=' btn btn-outline-dark w-100' onClick={aumentarCount}>+</button>
+				</div>
+			</div>
+			<div className="card-footer">
+				<button className='btn btn-outline-dark w-100' onClick={handleOnAdd}>Agregar al carrito</button>
+			</div>
+		</div>
+	)
+}
+
+export default ItemCount
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,14 +1,22 @@
+import ItemCount from "../ItemCount/ItemCount"
 import "./ItemDetail.css"
 
 const ItemDetail = ({ producto }) => {
+  const onAdd = (cant) => {
+    console.log(cant)
+  }
+
+
   return (
     <div className="item-detalle">
       <img src={producto.img} alt={producto.name} />
       <div className="item-body">
         <h2>{producto.name}</h2>
         <h4>Categoria: {producto.category}</h4>
-        <h4>Precio: {producto.price}</h4>
+        <h4>Precio: ${producto.price}</h4>
         <p>{producto.description}</p>
+
+        <ItemCount onAdd={onAdd}/>
       </div>
     </div>
   )
